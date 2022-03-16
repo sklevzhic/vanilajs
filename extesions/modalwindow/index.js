45bc719e00794134566554393dc65623ccea3a0b
Your Name <sklevzhic@gmail.com>
2022-03-16 17:27:35 +0300

    fix

---
+++ extesions/modalwindow/index.js
@@ -0,0 +1,13 @@
+let changeColor = document.getElementById('blackBg');
+
+
+if(changeColor != null) {
+    changeColor.addEventListener("click", getCode)
+
+}
+
+function getCode() {
+    return alert("|dsf")
+}
+
+
