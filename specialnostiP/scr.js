c1f75bd879028c121b8b196f0491900326162efe
Alexander <sklevzhic@gmail.com>
2022-03-27 21:37:08 +0300

    fix

--- specialnostiP/scr.js
+++ specialnostiP/scr.js
@@ -1,36 +1,117 @@
-let obj = { jsonTable: '[{"Тип":0,"Специальность переподготовки":"Практическая психология","Квалификация":"педагог-психолог\\n","онлайн":"идет набор группы","офлайн":"2022-05-15T21:00:00.000Z","заочная":"","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/pp.jpg","link":"/specialnosti-perepodgotovki/prakticheskaya-psihologiya"},{"Тип":0,"Специальность переподготовки":"Психология семейных отношений","Квалификация":"психолог\\n","онлайн":"2022-10-02T21:00:00.000Z","офлайн":"2022-10-09T21:00:00.000Z","заочная":"","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/pso.jpg","link":"/specialnosti-perepodgotovki/psihologiya-semeinyh-otnoshenii"},{"Тип":0,"Специальность переподготовки":"Психология","Квалификация":"психолог, преподаватель психологии\\n","онлайн":"2022-11-13T21:00:00.000Z","офлайн":"2022-10-09T21:00:00.000Z","заочная":"","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/psy.jpg","link":"/specialnosti-perepodgotovki/psihologiya"},{"Тип":0,"Специальность переподготовки":"Психология управления","Квалификация":"психолог-менеджер\\n","онлайн":"","офлайн":"","заочная":"идет набор группы","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/pu.jpg","link":"/specialnosti-perepodgotovki/psihologiya-upravleniya"},{"Тип":0,"Специальность переподготовки":"Педагогическая деятельность специалистов","Квалификация":"преподаватель в соответствии с квалификацией по основному образованию\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/photogall/DO,PP.png","link":"/specialnosti-perepodgotovki/pedagogicheskaya-deyatelnost-specialistov"},{"Тип":0,"Специальность переподготовки":"Социальная педагогика","Квалификация":"педагог социальный\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/sp.jpg","link":"/specialnosti-perepodgotovki/socialnaya-pedagogika"},{"Тип":1,"Специальность переподготовки":"Логопедия","Квалификация":"учитель-логопед\\n","онлайн":"","офлайн":"","заочная":"2022-10-23T21:00:00.000Z","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/logop.jpg","link":"/specialnosti-perepodgotovki/logopediya"},{"Тип":1,"Специальность переподготовки":"Интегрированное обучение и воспитание в дошкольном образовании","Квалификация":"учитель-дефектолог\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/ido.jpg","link":"/specialnosti-perepodgotovki/integrirovannoe-obuchenie-i-vospitanie-v-doshkolnom-obrazovanii"},{"Тип":1,"Специальность переподготовки":"Интегрированное обучение и воспитание в школьном образовании","Квалификация":"учитель-дефектолог\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/ish.jpg","link":"/specialnosti-perepodgotovki/integrirovannoe-obuchenie-i-vospitanie-v-shkolnom-obrazovanii"},{"Тип":1,"Специальность переподготовки":"Олигофренопедагогика","Квалификация":"олигофренопедагог\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/oligo.jpg","link":"/specialnosti-perepodgotovki/oligofrenopedagogika"},{"Тип":2,"Специальность переподготовки":"Дошкольное образование","Квалификация":"педагог\\n","онлайн":"","офлайн":"","заочная":"2022-11-13T21:00:00.000Z","очная (вечерняя)":"2022-04-24T21:00:00.000Z","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/do.jpg","link":"/specialnosti-perepodgotovki/doshkolnoe-obrazovanie"},{"Тип":2,"Специальность переподготовки":"Начальное образование","Квалификация":"учитель начальных классов\\n","онлайн":"","офлайн":"","заочная":"2022-10-16T21:00:00.000Z","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/no.jpg","link":"/specialnosti-perepodgotovki/nachalnoe-obrazovanie"},{"Тип":3,"Специальность переподготовки":"Иностранный язык","Квалификация":"преподаватель с указанием иностранного языка\\n","онлайн":"","офлайн":"","заочная":"идет набор группы","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/eng.jpg","link":"/specialnosti-perepodgotovki/inostrannyi-yazyk"},{"Тип":3,"Специальность переподготовки":"Информатика","Квалификация":"преподаватель информатики\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/inf.jpg","link":"/specialnosti-perepodgotovki/informatika"},{"Тип":3,"Специальность переподготовки":"Математика","Квалификация":"преподаватель математики\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/matem.jpg","link":"/specialnosti-perepodgotovki/matematika"},{"Тип":3,"Специальность переподготовки":"Физкультурно-оздоровительная работа в учреждениях образования","Квалификация":"преподаватель физической культуры\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/fo.jpg","link":"/specialnosti-perepodgotovki/fizkulturno-ozdorovitelnaya-rabota-v-uchrezhdeniyah-obrazovaniya-kvalifikaciya-prepodavatel-fizicheskoi-kultury"},{"Тип":4,"Специальность переподготовки":"Менеджмент учреждений дошкольного, общего среднего образования, дополнительного образования детей и молодежи","Квалификация":"менеджер в сфере образования\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/mo.jpg","link":"/specialnosti-perepodgotovki/menedzhment-uchrezhdenii-doshkolnogo-obshchego-srednego-obrazovaniya-dopolnitelnogo-obrazovaniya-detei-i-molodezhi"},{"Тип":5,"Специальность переподготовки":"Web-дизайн и компьютерная графика","Квалификация":"программист-Web-дизайнер\\n","онлайн":"","офлайн":"","заочная":"2022-09-11T21:00:00.000Z","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/webdes.png","link":"/specialnosti-perepodgotovki/web-dizain-i-kompyuternaya-grafika"}]' }
+let obj = {jsonTable: '[{"Тип":0,"Специальность переподготовки":"Практическая психология","Квалификация":"педагог-психолог\\n","онлайн":"идет набор группы","офлайн":"2022-05-15T21:00:00.000Z","заочная":"","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/pp.jpg","link":"/specialnosti-perepodgotovki/prakticheskaya-psihologiya"},{"Тип":0,"Специальность переподготовки":"Психология семейных отношений","Квалификация":"психолог\\n","онлайн":"2022-10-02T21:00:00.000Z","офлайн":"2022-10-09T21:00:00.000Z","заочная":"","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/pso.jpg","link":"/specialnosti-perepodgotovki/psihologiya-semeinyh-otnoshenii"},{"Тип":0,"Специальность переподготовки":"Психология","Квалификация":"психолог, преподаватель психологии\\n","онлайн":"2022-11-13T21:00:00.000Z","офлайн":"2022-10-09T21:00:00.000Z","заочная":"","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/psy.jpg","link":"/specialnosti-perepodgotovki/psihologiya"},{"Тип":0,"Специальность переподготовки":"Психология управления","Квалификация":"психолог-менеджер\\n","онлайн":"","офлайн":"","заочная":"идет набор группы","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/pu.jpg","link":"/specialnosti-perepodgotovki/psihologiya-upravleniya"},{"Тип":0,"Специальность переподготовки":"Педагогическая деятельность специалистов","Квалификация":"преподаватель в соответствии с квалификацией по основному образованию\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/photogall/DO,PP.png","link":"/specialnosti-perepodgotovki/pedagogicheskaya-deyatelnost-specialistov"},{"Тип":0,"Специальность переподготовки":"Социальная педагогика","Квалификация":"педагог социальный\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/sp.jpg","link":"/specialnosti-perepodgotovki/socialnaya-pedagogika"},{"Тип":1,"Специальность переподготовки":"Логопедия","Квалификация":"учитель-логопед\\n","онлайн":"","офлайн":"","заочная":"2022-10-23T21:00:00.000Z","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/logop.jpg","link":"/specialnosti-perepodgotovki/logopediya"},{"Тип":1,"Специальность переподготовки":"Интегрированное обучение и воспитание в дошкольном образовании","Квалификация":"учитель-дефектолог\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/ido.jpg","link":"/specialnosti-perepodgotovki/integrirovannoe-obuchenie-i-vospitanie-v-doshkolnom-obrazovanii"},{"Тип":1,"Специальность переподготовки":"Интегрированное обучение и воспитание в школьном образовании","Квалификация":"учитель-дефектолог\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/ish.jpg","link":"/specialnosti-perepodgotovki/integrirovannoe-obuchenie-i-vospitanie-v-shkolnom-obrazovanii"},{"Тип":1,"Специальность переподготовки":"Олигофренопедагогика","Квалификация":"олигофренопедагог\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/oligo.jpg","link":"/specialnosti-perepodgotovki/oligofrenopedagogika"},{"Тип":2,"Специальность переподготовки":"Дошкольное образование","Квалификация":"педагог\\n","онлайн":"","офлайн":"","заочная":"2022-11-13T21:00:00.000Z","очная (вечерняя)":"2022-04-24T21:00:00.000Z","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/do.jpg","link":"/specialnosti-perepodgotovki/doshkolnoe-obrazovanie"},{"Тип":2,"Специальность переподготовки":"Начальное образование","Квалификация":"учитель начальных классов\\n","онлайн":"","офлайн":"","заочная":"2022-10-16T21:00:00.000Z","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/no.jpg","link":"/specialnosti-perepodgotovki/nachalnoe-obrazovanie"},{"Тип":3,"Специальность переподготовки":"Иностранный язык","Квалификация":"преподаватель с указанием иностранного языка\\n","онлайн":"","офлайн":"","заочная":"идет набор группы","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/eng.jpg","link":"/specialnosti-perepodgotovki/inostrannyi-yazyk"},{"Тип":3,"Специальность переподготовки":"Информатика","Квалификация":"преподаватель информатики\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/inf.jpg","link":"/specialnosti-perepodgotovki/informatika"},{"Тип":3,"Специальность переподготовки":"Математика","Квалификация":"преподаватель математики\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/matem.jpg","link":"/specialnosti-perepodgotovki/matematika"},{"Тип":3,"Специальность переподготовки":"Физкультурно-оздоровительная работа в учреждениях образования","Квалификация":"преподаватель физической культуры\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/fo.jpg","link":"/specialnosti-perepodgotovki/fizkulturno-ozdorovitelnaya-rabota-v-uchrezhdeniyah-obrazovaniya-kvalifikaciya-prepodavatel-fizicheskoi-kultury"},{"Тип":4,"Специальность переподготовки":"Менеджмент учреждений дошкольного, общего среднего образования, дополнительного образования детей и молодежи","Квалификация":"менеджер в сфере образования\\n","онлайн":"","офлайн":"","заочная":"январь 2023","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/mo.jpg","link":"/specialnosti-perepodgotovki/menedzhment-uchrezhdenii-doshkolnogo-obshchego-srednego-obrazovaniya-dopolnitelnogo-obrazovaniya-detei-i-molodezhi"},{"Тип":5,"Специальность переподготовки":"Web-дизайн и компьютерная графика","Квалификация":"программист-Web-дизайнер\\n","онлайн":"","офлайн":"","заочная":"2022-09-11T21:00:00.000Z","очная (вечерняя)":"","photo":"/admin-panel/vendor/kcfinder/upload/images/specialnosti_perepodgotovki/webdes.png","link":"/specialnosti-perepodgotovki/web-dizain-i-kompyuternaya-grafika"}]'}
 
 
+function getCode() {
+    let strings = {
+        0: "Специальности психолого-педагогической деятельности",
+        1: "Специальности в области специального образования",
+        2: "Специальности в сфере дошкольного и начального образования",
+        3: "Специальности в области преподавания учебных предметов на уровне общего среднего образования",
+        4: "Специальности в области менеджмента",
+        5: "Специальности в области рекламы, информационных технологий и социокультурного обеспечения туристической деятельности",
+    }
 
 
+    let res = ""
 
-let strings = {
-    0: "Специальности психолого-педагогической деятельности",
-    1: "Специальности в области специального образования",
-    2: "Специальности в сфере дошкольного и начального образования",
-    3: "Специальности в области преподавания учебных предметов на уровне общего среднего образования",
-    4: "Специальности в области менеджмента",
-    5: "Специальности в области рекламы, информационных технологий и социокультурного обеспечения туристической деятельности",
+
+    let specialities = JSON.parse(obj.jsonTable)
+
+    const groups = Object.keys(strings).map(key => {
+        return specialities.filter(el => el["Тип"] == key)
+    })
+
+
+    groups.forEach(el => {
+        res += `<div>
+       <b>${strings[el[0]["Тип"]]}</b>
+       <div>${renderSpecialities(el)}</div>
+</div>`
+    })
+
+
+    function renderSpecialities(arr) {
+
+        return arr.map(el => {
+            return `<div>начало обучения:${getDates(el)} - ${el["Специальность переподготовки"]}</div>`
+        }).join("")
+    }
+
+    function getDates(obj) {
+        let values = ["заочная","онлайн","офлайн","очная (вечерняя)"]
+        let a = values.map(el => {
+            if (!obj[el]) {
+                return null
+            }
+            return {
+                [el]: (new RegExp(/[а-яА-Я]/g).test(obj[el])) ? obj[el] : formatDate(obj[el])
+            }
+        }).filter(n => n)
+
+        return a.map(obj => {
+            return Object.keys(obj).map(key => {
+                return ` ${key} ${obj[key]}`
+            })
+        }).join(", ")
+    }
+
+    function formatDate(date) {
+        let d = new Date(Date.parse(date))
+        let day = d.getDate() + 1
+        let month = ((d.getMonth() + 1) <=9 ) ? "0"+d.getMonth() + 1 : d.getMonth() + 1
+        let year = d.getFullYear()
+
+        return day + "." + month + "." + year
+    }
+
+    // return res
+
+    document.querySelector("#iddd").innerHTML = res
+
+
 }
 
+getCode()
+
+
+// let dateExel = 44817
+today = Date.now()
 
 
 
+// 1000*60*60*(24*19248)
 
 
-let specialities = JSON.parse(obj.jsonTable)
+function newDate(v) {
+    let dateStartMS = 25569
+    return (+v > 40000) ? 1000*60*60*(24*(+v - dateStartMS)) : v
+}
 
-const groups = Object.keys(strings).map(key => {
-    return specialities.filter(el => el["Тип"] == key)
-})
-
-console.log(groups)
-
-let res = ""
-
-specialities.map(el => {
-    res += `<h2>111</h2>`
-})
+console.log(newDate("идет набор группы"))
+console.log(newDate("январь 2023"))
+console.log(newDate("24.10.2022"))
+console.log(newDate("44817"))
+console.log(newDate(""))
 
 
-document.querySelector("#iddd").innerHTML = res
+function formatDate1(date) {
+
+    let d = (+date > 1000000000) ? new Date(+date) : new Date(Date.parse(date))
+    let day = d.getDate() + 1
+    let month = ((d.getMonth() + 1) <=9 ) ? "0"+(d.getMonth() + 1) : d.getMonth() + 1
+    let year = d.getFullYear()
+
+    return day + "." + month + "." + year
+}
+
+console.log(formatDate1("1662940800000"))
+
+name = "name"
+lastname = "lastname"
+let data = {name, lastname}
+
+
+
+for (let key1 in data) {
+    console.log(data.name)
+}
